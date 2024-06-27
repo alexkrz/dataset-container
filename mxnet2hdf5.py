2a909b5e5caa162c8447d2df21b6906e30de565a
Alexander Kurz <alex.kurz95@gmail.com>
2024-06-27 13:34:04 +0200

    Write images in jpeg bytes to hdf5 container

--- mxnet2hdf5.py
+++ mxnet2hdf5.py
@@ -1,3 +1,4 @@
+import io
 import os
 from pathlib import Path
 
@@ -5,6 +6,7 @@ import h5py
 import matplotlib.pyplot as plt
 import mxnet as mx
 import numpy as np
+from PIL import Image
 from tqdm import tqdm
 
 
@@ -32,17 +34,19 @@ def main(
 
     # Load images and labels from mxnet to numpy arrays
     h5file = h5py.File(hdf5_fp, "w")
-    image_dataset = h5file.create_dataset(
-        name="images",
-        shape=(len(imgidx), 112, 112, 3),
-        dtype=np.uint8,
-    )
-    label_dataset = h5file.create_dataset(
-        name="labels",
-        shape=(len(imgidx),),
-        dtype=np.int64,
-    )
-    for i in tqdm(range(len(imgidx))):
+    # image_dataset = h5file.create_dataset(
+    #     name="images",
+    #     shape=(len(imgidx), 112, 112, 3),
+    #     dtype=np.uint8,
+    # )
+    # label_dataset = h5file.create_dataset(
+    #     name="labels",
+    #     shape=(len(imgidx),),
+    #     dtype=np.int64,
+    # )
+    h5group = h5file.create_group("images")
+    # for i in tqdm(range(len(imgidx))):
+    for i in tqdm(range(1000)):
         # fname = f"{idx:0{digits_img}d}"
         # print(fname)
         idx = i + 1
@@ -55,9 +59,25 @@ def main(
         else:
             raise RuntimeError("Unexpected label dtype")
         img_arr = mx.image.imdecode(img).asnumpy()
-        img_bytes = img_arr.tobytes()
-        image_dataset[i] = img_arr
-        label_dataset[i] = label
+        # img_bytes = img_arr.tobytes()
+
+        # Convert the NumPy array to a PIL Image
+        pil_image = Image.fromarray(img_arr)
+
+        # Create a BytesIO object to hold the JPEG bytes
+        jpeg_buffer = io.BytesIO()
+
+        # Save the PIL Image as a JPEG into the BytesIO object
+        pil_image.save(jpeg_buffer, format="JPEG")
+
+        # Get the JPEG bytes
+        jpeg_bytes = jpeg_buffer.getvalue()
+        binary_data_np = np.asarray(jpeg_bytes)
+
+        dset = h5group.create_dataset(f"{idx:0{digits_img}d}", data=binary_data_np)
+
+        # image_dataset[i] = img_arr
+        # label_dataset[i] = label
 
     h5file.close()
 
