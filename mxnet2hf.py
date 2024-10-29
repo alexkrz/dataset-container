c3a8cade887e7b311a4f9029ab93fb172c39228b
Alexander Kurz <alex.kurz95@gmail.com>
2024-10-29 15:06:06 +0100

    Try to convert balanceface dataset

--- mxnet2hf.py
+++ mxnet2hf.py
@@ -10,20 +10,21 @@ from PIL import Image
 
 
 def main(
-    mxnet_dir: Path = Path.home() / "Data" / "TrainDatasets" / "casia_webface-mxnet",
-    out_dir: Path = Path.home() / "Data" / "TrainDatasets" / "casia_webface-parquet",
+    mxnet_dir: Path = Path.home() / "Data" / "FairnessBias" / "bupt_balance",
+    out_dir: Path = Path.home() / "Data" / "FairnessBias" / "bupt_balance-parquet",
+    num_shards: int = 10,
 ):
-
     assert mxnet_dir.exists()
 
-    def entry_for_id(imgrec, idx):
+    def entry_for_id(idx):
         s = imgrec.read_idx(idx)
         header, img_bytes = mx.recordio.unpack(s)
         label = header.label
-        if isinstance(label, float):
-            label = int(label)
-        else:
-            raise RuntimeError("Unexpected label dtype")
+        # assert isinstance(label, float)
+        # label = int(label)
+        assert isinstance(label, np.ndarray)
+        identity = int(label[0])
+        ethnicity = int(label[1])  # TODO: label[1] is always 1. Where is ethnicity encoded?
         # img_arr = mx.image.imdecode(img).asnumpy()
         pil_image = Image.open(io.BytesIO(img_bytes))
         # plt.imshow(pil_image)
@@ -31,27 +32,30 @@ def main(
         # plt.show()
         return {
             "img": pil_image,
-            "label": label,
+            "identity": identity,
+            "ethnicity": ethnicity,
         }
 
     def generate_entries():
-        path_imgrec = str(mxnet_dir / "train.rec")
-        path_imgidx = str(mxnet_dir / "train.idx")
-        imgrec = mx.recordio.MXIndexedRecordIO(path_imgidx, path_imgrec, "r")
-        s = imgrec.read_idx(0)
-        header, _ = mx.recordio.unpack(s)
-        if header.flag > 0:
-            header0 = (int(header.label[0]), int(header.label[1]))
-            imgidx = np.array(range(1, int(header.label[0])))
-        else:
-            imgidx = np.array(list(imgrec.keys))
-
         for i in range(len(imgidx)):
             idx = i + 1
-            yield entry_for_id(imgrec, idx)
+            yield entry_for_id(idx)
+
+    path_imgrec = str(mxnet_dir / "train.rec")
+    path_imgidx = str(mxnet_dir / "train.idx")
+    imgrec = mx.recordio.MXIndexedRecordIO(path_imgidx, path_imgrec, "r")
+    s = imgrec.read_idx(0)
+    header, _ = mx.recordio.unpack(s)
+    if header.flag > 0:
+        header0 = (int(header.label[0]), int(header.label[1]))
+        imgidx = np.array(range(1, int(header.label[0])))
+    else:
+        imgidx = np.array(list(imgrec.keys))
+
+    print("Number of images:", len(imgidx))
 
     ds = Dataset.from_generator(generate_entries)
-    num_shards = 3
+
     for index in range(num_shards):
         shard = ds.shard(index=index, num_shards=num_shards, contiguous=True)
         shard.to_parquet(out_dir / f"train-{(index+1):0{5}d}-of-{num_shards:0{5}d}.parquet")
