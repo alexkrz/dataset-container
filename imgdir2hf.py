c809181380486a501f0ffe0471354b7f41c9b364
Alexander Kurz <alex.kurz95@gmail.com>
2025-08-20 11:45:51 +0200

    Convert b3fd dataset

--- imgdir2hf.py
+++ imgdir2hf.py
@@ -1,4 +1,5 @@
 import io
+import os
 from pathlib import Path
 
 import datasets
@@ -9,27 +10,30 @@ from tqdm import tqdm
 
 
 def main(
-    data_dir: Path = Path.home() / "Data" / "EmotionRecognition" / "AffectNet",
-    out_dir: Path = Path.home() / "Data" / "EmotionRecognition" / "AffectNet-parquet",
-    split: str = "test",
+    data_dir: str = os.environ["HOME"] + "/Data/Experimental/B3FD/B3FD_images/B3FD",
+    out_dir: str = os.environ["HOME"] + "/Data/Experimental/B3FD/B3FD_parquet",
+    name: str = "b3fd",
 ):
     # ds = datasets.load_dataset("imagefolder", data_dir=str(data_dir / f"{split}"), split="train")
     # print(ds)
     # ds.to_parquet(out_dir / f"{split}.parquet")
-    data_dir = data_dir / split
+    data_dir = Path(data_dir)  # type: Path
+    out_dir = Path(out_dir)  # type: Path
+
     subdirs = sorted(list(data_dir.glob("*")))
     entries = []
-    for subdir in subdirs:
+    for subdir in tqdm(subdirs):
         label = subdir.name
-        label = int(label)
-        print("Label:", label)
+        # label = int(label)
+        # print("Label:", label)
         img_paths = sorted(list(subdir.glob("*.jpg")))
-        for img_p in tqdm(img_paths):
+        for img_p in img_paths:
             pil_img = Image.open(img_p)
             jpeg_buffer = io.BytesIO()
             pil_img.save(jpeg_buffer, format="JPEG")
             img_bytes = jpeg_buffer.getvalue()
-            entry = {"img": img_bytes, "label": label}
+            rel_path = str(img_p.relative_to(data_dir))
+            entry = {"img": img_bytes, "label": label, "path": rel_path}
             entries.append(entry)
     df = pd.DataFrame(entries)
     # df = df.sort_values(by="label", ignore_index=True)
@@ -40,7 +44,7 @@ def main(
     ds = Dataset.from_pandas(df)
     ds = ds.cast_column("img", datasets.Image())
 
-    ds.to_parquet(out_dir / f"{split}.parquet")
+    ds.to_parquet(out_dir / f"{name}.parquet")
 
 
 if __name__ == "__main__":
